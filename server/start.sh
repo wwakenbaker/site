028b5bfac4d12e732e98254a6b25e7e8c806889e
work <smashleader666@gmail.com>
2025-01-24 13:23:39 +0300

    deploy

--- server/start.sh
+++ server/start.sh
@@ -2,4 +2,4 @@
 
 python init_db.py
 
-exec gunicorn app:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000
+exec gunicorn app:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000 --log-level=error
